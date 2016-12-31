7f15525cb7ec0dfdd1e14dee0025a04313265d00
Atsuo Ishimoto <ishimoto@gembook.org>
2016-12-31 18:18:18 +0900

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setup(
     name="happylogging",
-    version="0.0.1",
+    version="0.0.2",
     author="Atsuo Ishimoto",
     description="Utility functions to help using standard logging module.",
     license="MIT",
